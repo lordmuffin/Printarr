cb805ef6e3a02e5400e2eb557f3c298afafc9da0
Andrew Jackson <andrew@healingorganics.org>
2026-03-22 08:11:35 -0500

    feat: Add Terraform provider requirements for proxmox and null providers.

---
+++ tofu/modules/printarr-lxc/versions.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "~> 0.66"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
+  }
+}
